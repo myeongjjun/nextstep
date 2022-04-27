d0cfea769c453637a2278a960371ef4c72d82cd7
myeongjjun <mjk3026@gmail.com>
2022-04-27 23:44:43 +0900

    테스트 케이스 추가 - custom, 기본 구분자를 혼용하는 케이스 - 구분자 정보가 없는 경우 0값 처리하는 케이스

--- src/main/java/me/myjju/nextstep/ch2/StringCalculator.java
+++ src/main/java/me/myjju/nextstep/ch2/StringCalculator.java
@@ -1,6 +1,9 @@
 package me.myjju.nextstep.ch2;
 
+import org.springframework.util.StringUtils;
+
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -9,39 +12,39 @@ import java.util.stream.Collectors;
 public class StringCalculator {
 
     private static final Pattern CUSTOM_STRING_PATTERN = Pattern.compile("^(?:\\/\\/(.)\\n)?(.+)");
-    private static String BASIC_SEPARATOR = "\\,|\\:";
+    private static final String BASIC_SEPARATOR = "\\,|\\:";
 
     public int add(String input) throws RuntimeException {
-
-        if (input == null || input.isEmpty()) {
+        // 입력값이 비어있으면
+        if (isBlank(input)) {
             return 0;
         }
 
+        // 입력값이 숫자로 변환이 되면
         try {
             return Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-
+        } catch (NumberFormatException ignored) {
         }
 
-        Matcher matcher = CUSTOM_STRING_PATTERN.matcher(input);
-        if (!matcher.find()) {
-            return 0;
-        }
-
-        String separator = getSeparator(matcher);
-        String data = matcher.group(2);
-
-        int result = 0;
+        // 입력값 구분자, 합계 처리
         try {
-            Calculator calculator = Calculator.create(separator, data);
-            result = calculator.operate();
+            return operate(toIntegerList(input));
         } catch (NumberFormatException e) { // 구분된 문자열이 숫자가 아닌경우
-
         }
 
-        return result;
+        return 0;
     }
 
+    private boolean isBlank(String input) {
+        return input == null || input.isBlank();
+    }
+
+    /**
+     * 입력값을 바탕으로 구분자를 얻는 메서드
+     *
+     * @param matcher
+     * @return 구분자
+     */
     private String getSeparator(Matcher matcher) {
         String customSplitChar = matcher.group(1);
         if (customSplitChar == null) {
@@ -51,32 +54,27 @@ public class StringCalculator {
     }
 
 
-    static class Calculator {
-        private final List<Integer> lists;
-
-        private Calculator(List<Integer> lists) {
-            this.lists = lists;
-        }
-
-        public static Calculator create(String separator, String data) {
+    public List<Integer> toIntegerList(String input) {
+        Matcher matcher = CUSTOM_STRING_PATTERN.matcher(input);
+        if (matcher.find()) {
+            String separator = getSeparator(matcher);
+            String data = matcher.group(2);
             String[] split = data.split(separator);
-            List<Integer> lists = Arrays.stream(split).map(Calculator::parseUnsignedInt)
+            return Arrays.stream(split).map(this::parseUnsignedInt)
                     .collect(Collectors.toList());
-            return new Calculator(lists);
         }
-
-        private static Integer parseUnsignedInt(String value) {
-            int i = Integer.parseInt(value);
-            if (i < 0) {
-                throw new RuntimeException("음수 예외");
-            }
-            return i;
-        }
-
-        public int operate() {
-            return lists.stream().mapToInt(Integer::intValue).sum();
-        }
+        return Collections.emptyList();
     }
 
+    private Integer parseUnsignedInt(String value) {
+        int i = Integer.parseInt(value);
+        if (i < 0) {
+            throw new RuntimeException("음수 예외");
+        }
+        return i;
+    }
 
+    public int operate(List<Integer> lists) {
+        return lists.stream().mapToInt(Integer::intValue).sum();
+    }
 }
--- src/test/java/me/myjju/nextstep/ch2/StringCalculatorTest.java
+++ src/test/java/me/myjju/nextstep/ch2/StringCalculatorTest.java
@@ -3,6 +3,9 @@ package me.myjju.nextstep.ch2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 class StringCalculatorTest {
@@ -23,6 +26,7 @@ class StringCalculatorTest {
     @Test
     void add_null_또는_빈문자() {
         assertThat(calculator.add(" ")).isEqualTo(0);
+        assertThat(calculator.add(null)).isEqualTo(0);
     }
 
     @Test
@@ -46,15 +50,41 @@ class StringCalculatorTest {
 
         assertThat(calculator.add("//;\n1;2;3")).isEqualTo(6);
         assertThat(calculator.add("//*\n1*2*3")).isEqualTo(6);
+    }
+
+    @Test
+    void add_custom_쉼표_콜론_구분자() {
+
+        assertThat(calculator.add("//;\n1;2;3")).isEqualTo(6);
+        assertThat(calculator.add("//*\n1*2:3,4")).isEqualTo(10);
+    }
+
+    @Test
+    void 알수없는_구분자_0처리() {
+
         assertThat(calculator.add("//*\n1*2#3")).isEqualTo(0);
     }
 
     @Test
-    void sumTest() {
+    void 음수는_예외처리() {
         assertThatThrownBy(() -> calculator.add("1,-1")).isInstanceOf(RuntimeException.class);
-        assertThatThrownBy(() ->calculator.add("//;\n1;-1;8")).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> calculator.add("//;\n1;-1;8")).isInstanceOf(RuntimeException.class);
     }
 
+    @Test
+    void split_test() {
+        String[] split = "1*2;3,4,".split("\\,|\\:");
+        for (String s : split) {
+            System.out.println("s = " + s);
+        }
+    }
 
+    @Test
+    void empty한_Integer컬렉션의_합의_결과는_0이다() {
+        List<Integer> list = Collections.emptyList();
+        int sum = list.stream().mapToInt(Integer::intValue)
+                .sum();
+        assertThat(sum).isEqualTo(0);
+    }
 
 }
